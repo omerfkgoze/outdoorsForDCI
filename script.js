00e2ee207fad9c7357ba1539e0b006ce3f73c080
omerfkgoze <farukucukgoze@gmail.com>
2023-05-01 19:18:47 +0200

    update

--- script.js
+++ script.js
@@ -12,16 +12,3 @@ function removeActiveClasses() {
     panels[i].classList.remove('active');
   }
 }
-
-// panels.forEach(panel => {
-//   panel.addEventListener('click', () => {
-//     removeActiveClasses();
-//     panel.classList.add('active');
-//   });
-// });
-
-// function removeActiveClasses() {
-//   panels.forEach(panel => {
-//     panel.classList.remove('active');
-//   });
-// }
